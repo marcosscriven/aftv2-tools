1d3957e2aedd09f75788405d1eb80656e5191adf
Eric Work <work.eric@gmail.com>
2015-12-19 09:40:28 -0800

    Clean up patch_mmc.bat a little (also fix some comments)

--- patch_mmc.bat
+++ patch_mmc.bat
@@ -1,18 +1,11 @@
 @echo off
-rem #!/bin/bash
-
-rem  check usage
-rem if [ %3 !=  ] ; then
-rem     echo "Usage: $(basename $0) base_addr base_img diff_file"
-rem     exit 1
-rem fi
 
 set BASE_ADDR=0x00000000058e0000
 set BASE_IMG=system.root.img
 set DIFF_FILE=system.diff
 
 rem Start program before plugging in device, handshake first..
-rem handshake_win.py
+rem handshake.py
 
 rem # get changed regions
 changed_blks.py %DIFF_FILE% > changed.txt
@@ -20,15 +13,16 @@ changed_blks.py %DIFF_FILE% > changed.txt
 rem # iterate over regions
 for /f "tokens=1,2" %%A in (changed.txt) do call :subroutine %%A %%B
 
-Echo Done!
+echo Done!
 pause
 exit /b
 
 :subroutine
+
     set ADDR=%1
     set LENGTH=%2
     set /a PHYS_ADDR= %BASE_ADDR% + %ADDR%
-    # convert PHYS_ADDR to hex
+    rem # convert PHYS_ADDR to hex
     call cmd /c exit /b %PHYS_ADDR%
     set PHYS_ADDRx=%=exitcode%
 
@@ -43,7 +37,6 @@ exit /b
     write_mmc.py 0x%PHYS_ADDRx% %TMP_FILE%
 
     rem # patch success?
-
     if  %errorlevel% GEQ 1 echo error %errorlevel%
     echo.
     exit /b
